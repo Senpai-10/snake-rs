3977e0ab8bee992a510a32bec207acd27abfdb04
senpai-10 <bmjfdrh@gmail.com>
2022-05-15 08:45:41 +0300

    remove window struct

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,18 @@ mod game;
 use game::Game;
 use ncurses::*;
 
-struct Window {
-    y_max: i32,
-    x_max: i32,
-}
-
 fn main() {
     initscr();
     noecho();
     cbreak();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
-    let mut window = Window { x_max: 0, y_max: 0 };
-    getmaxyx(stdscr(), &mut window.y_max, &mut window.x_max);
+    let mut y_max = 0;
+    let mut x_max = 0;
 
-    let game_window = newwin(
-        window.y_max / 2,
-        window.x_max / 2,
-        window.y_max / 4,
-        window.x_max / 4,
-    );
+    getmaxyx(stdscr(), &mut y_max, &mut x_max);
+
+    let game_window = newwin(y_max / 2, x_max / 2, y_max / 4, x_max / 4);
     box_(game_window, 0, 0);
 
     let mut game = Game::init(game_window);
